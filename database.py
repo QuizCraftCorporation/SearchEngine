23a849583f76d5a1e4d4fe3788a6e066209d12c8
Nagim <39875672+Nagim123@users.noreply.github.com>
2023-07-16 13:58:57 +0300

    Initial upload

---
+++ database.py
@@ -0,0 +1,15 @@
+class TextBase():
+    def __init__(self) -> None:
+        pass
+
+    def search():
+        pass
+
+    def add_new():
+        pass
+
+    def delete():
+        pass
+
+    def clear():
+        pass
