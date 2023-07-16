dc12591a963b47c71a66b69cb21a856f3aeb0b12
Nagim <39875672+Nagim123@users.noreply.github.com>
2023-07-16 16:05:29 +0300

    Add http server

--- database.py
+++ database.py
@@ -1,18 +1,43 @@
+import os
+import pathlib
 from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.schema import Document
 
-class TextBase():
+SAVE_FOLDER = os.path.join(pathlib.Path(__file__).parent.resolve(), "./vector_store")
+
+class TextDataBase():
     def __init__(self) -> None:
-        self.embeddings = HuggingFaceEmbeddings(cache_folder="models/all-mpnet-base-v2")
+        self.embeddings = HuggingFaceEmbeddings(model_name="./models/all-mpnet-base-v2")
+        self.db = None
+        if os.path.exists(os.path.join(SAVE_FOLDER, "index.faiss")):
+            self.db = FAISS.load_local(SAVE_FOLDER, embeddings=self.embeddings)
 
-    def search():
-        pass
+    def search(self, query: str, number_of_results: int) -> tuple:
+        if self.db == None:
+            return ("empty", [])
 
-    def add_new():
-        pass
+        docs = self.db.similarity_search(query, k=number_of_results)
+        the_most_similar_quizzes = []
+        for doc in docs:
+            quiz = doc.page_content
+            the_most_similar_quizzes.append((quiz, doc.metadata["unique_name"]))
+        
+        return ("ok", the_most_similar_quizzes)
 
-    def delete():
-        pass
+    def add_text(self, text: str, unique_name: str) -> None:
+        document = Document(page_content=text, metadata=dict(unique_name=unique_name))
 
-    def clear():
-        pass
+        if self.db == None:
+            self.db = FAISS.from_documents([document], embedding=self.embeddings)
+        else:
+            self.db.add_documents([document])
+        self.db.save_local(SAVE_FOLDER)
 
+
+# test = TextDataBase()
+# test.add_text("Mother finger mother finger", "1")
+# test.add_text("Johny Johny yes papa", "2")
+# test.add_text("Crystals is very style appropriate", "3")
+
+# print(test.search("Woman", 3))
---
+++ server.py
@@ -0,0 +1,57 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+from database import TextDataBase
+import json
+
+HOST = "127.0.0.1"
+PORT = 1234
+
+DATABASE = TextDataBase()
+
+class DatabaseHTTP(BaseHTTPRequestHandler):
+    
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/plain")
+        self.end_headers()
+        self.wfile.write(bytes("Everything is up!", "utf-8"))
+    
+    def do_POST(self):
+        self.send_response(200)
+        self.send_header("Content-type", "application/json")
+        self.end_headers()
+        if self.path == "/save":
+            user_data = json.loads(self.rfile.read(int(self.headers['Content-Length'])))
+            quiz_payload = user_data["raw_quiz_data"]
+            unique_name = user_data["unique_id"]
+            DATABASE.add_text(quiz_payload, unique_name)
+            response = {}
+            response["operation"] = "SUCCESS"
+            self.wfile.write(bytes(json.dumps(response), "utf-8"))
+        elif self.path == "/search":
+            user_data = json.loads(self.rfile.read(int(self.headers['Content-Length'])))
+            query = user_data["query"]
+            k = user_data["number_of_results"]
+            results = DATABASE.search(query, k)
+            response = {}
+            if results[0] == "ok":
+                quizzes_data = []
+                for result in results[1]:
+                    quiz_data = {}
+                    quiz_data["raw_quiz_data"] = result[0]
+                    quiz_data["unique_id"] = result[1]
+                    quizzes_data.append(quiz_data)    
+                response["operation"] = "SUCCESS"
+                response["payload"] = quizzes_data
+            else:
+                response["operation"] = "FAILED"
+            self.wfile.write(bytes(json.dumps(response), "utf-8"))
+        else:
+            response = {}
+            response["operation"] = "FAILED"
+            self.wfile.write(bytes(json.dumps(response), "utf-8"))
+
+server = HTTPServer((HOST, PORT), DatabaseHTTP)
+print(f"SERVER RUNNED ON {HOST}:{PORT}")
+server.serve_forever()
+server.server_close()
+print("SERVER STOPPED :C")
